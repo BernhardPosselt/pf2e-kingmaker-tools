9d65108f51a24b741d890940c39456eaad894b1f
Bernhard Posselt <bernhard@posselt.at>
2023-06-11 17:10:22 +0200

    migrate remove content script

--- package.sh
+++ package.sh
@@ -21,7 +21,11 @@ node ./scripts/update-module-json.mjs "$version"
 cp module.json map.json README.md LICENSE CHANGELOG.md OpenGameLicense.md build/pf2e-kingmaker-tools/
 cp -r packs/ docs/ templates/ styles/ dist/ build/pf2e-kingmaker-tools/
 
+# remove broken links
+./node_modules/.bin/fvtt package workon "pf2e-kingmaker-tools"
+./node_modules/.bin/fvtt package unpack "kingmaker-tools-random-encounters" --od build/tmp/kingmaker-tools-random-encounters
 node ./scripts/remove-content.mjs
+./node_modules/.bin/fvtt package pack "kingmaker-tools-random-encounters" --id build/tmp/kingmaker-tools-random-encounters --od build/pf2e-kingmaker-tools/packs
 
 cd build
 zip -r release.zip pf2e-kingmaker-tools
