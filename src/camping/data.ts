904b39d2ff14a55a0605412b5c8e314ecf1c035f
Bernhard Posselt <bernhard@posselt.at>
2023-07-04 01:05:20 +0200

    add types

--- src/camping/data.ts
+++ src/camping/data.ts
@@ -6,3 +6,10 @@ export const rationUuid = 'Compendium.pf2e.equipment-srd.Item.L9ZV076913otGtiB';
 export type CombatEffectCompanions = 'Amiri' | 'Nok-Nok' | 'Jaethal' | 'Kalikke';
 
 export type Proficiency = 'trained' | 'expert' | 'master' | 'legendary';
+
+export const allowedActors = new Set([
+    'character',
+    'npc',
+    'loot',
+    'vehicle',
+]);
