489844a4678056c3d1d53b891721821c1579fe63
Bernhard Posselt <bernhard@posselt.at>
2024-02-08 23:59:08 +0100

    * Remove double none dc setting for kingdom activities and add **custom** which sets the dc to 0

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -5,6 +5,10 @@ All notable changes to this project will be documented in this file.
 The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
 and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
+### Fixed
+
+* Remove double none dc setting for kingdom activities and add **custom** which sets the dc to 0
+
 ## [0.29.0] - 2024-02-08
 
 ### Added
--- src/kingdom/dialogs/add-activity-dialog.ts
+++ src/kingdom/dialogs/add-activity-dialog.ts
@@ -139,7 +139,7 @@ class AddKingdomActivities extends FormApplication<FormApplicationOptions & AddK
             success: this.activity.success?.msg ?? '',
             failure: this.activity.failure?.msg ?? '',
             criticalFailure: this.activity.criticalFailure?.msg ?? '',
-            dcs: ['none', 'control', ...range(14, 51).map(i => i.toString())]
+            dcs: ['none', 'control', 'custom', ...range(14, 51).map(i => i.toString())]
                 .map(v => {
                     return {
                         value: v,
