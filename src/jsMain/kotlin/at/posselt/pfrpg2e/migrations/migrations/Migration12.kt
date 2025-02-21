c619c33e18f1ac223adc1e547b69ccc66b0337c6
Bernhard Posselt <bernhard@posselt.at>
2025-02-21 17:16:49 +0100

    add new leadership variant for v&k

--- src/jsMain/kotlin/at/posselt/pfrpg2e/migrations/migrations/Migration12.kt
+++ src/jsMain/kotlin/at/posselt/pfrpg2e/migrations/migrations/Migration12.kt
@@ -5,6 +5,6 @@ import com.foundryvtt.core.Game
 
 class Migration12 : Migration(12) {
     override suspend fun migrateKingdom(game: Game, kingdom: KingdomData) {
-        kingdom.activityBlacklist = kingdom.activityBlacklist + arrayOf("focused-attention-vk", "celebrate-holiday-vk", "retrain-vk", "fortify-hex-vk", "clear-hex-vk")
+        kingdom.activityBlacklist = kingdom.activityBlacklist + arrayOf("focused-attention-vk", "celebrate-holiday-vk", "retrain-vk", "fortify-hex-vk", "clear-hex-vk", "new-leadership-vk")
     }
 }
