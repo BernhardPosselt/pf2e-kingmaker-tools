c180d0de76e6256085310ad10ff4056a424fa219
Bernhard Posselt <bernhard@posselt.at>
2025-03-30 11:48:50 +0200

    add settlement picker

---
+++ src/jsMain/kotlin/at/posselt/pfrpg2e/kingdom/dialogs/PickEventSettlement.kt
@@ -0,0 +1,53 @@
+package at.posselt.pfrpg2e.kingdom.dialogs
+
+import at.posselt.pfrpg2e.app.awaitablePrompt
+import at.posselt.pfrpg2e.app.forms.CheckboxInput
+import at.posselt.pfrpg2e.app.forms.FormElementContext
+import at.posselt.pfrpg2e.app.forms.Select
+import at.posselt.pfrpg2e.app.forms.SelectOption
+import at.posselt.pfrpg2e.app.forms.formContext
+import at.posselt.pfrpg2e.data.kingdom.settlements.Settlement
+import kotlinx.js.JsPlainObject
+
+
+@JsPlainObject
+external interface PickEventSettlementContext {
+    val formRows: Array<FormElementContext>
+}
+
+@JsPlainObject
+external interface PickEventSettlementData {
+    val settlementId: String
+    val secretLocation: Boolean
+}
+
+data class PickEventSettlementResult(
+    val settlementId: String,
+    val secretLocation: Boolean,
+)
+
+suspend fun pickEventSettlement(
+    settlements: List<Settlement>,
+): PickEventSettlementResult {
+    return awaitablePrompt<PickEventSettlementData, PickEventSettlementResult>(
+        title = "Choose a Settlement",
+        templateContext = PickEventSettlementContext(
+            formRows = formContext(
+                Select(
+                    name = "settlementId",
+                    label = "Settlement",
+                    options = settlements.map { SelectOption(value = it.id, label = it.name) }
+                ),
+                CheckboxInput(
+                    name = "secretLocation",
+                    label = "Hide Settlement from Players",
+                    value = false,
+                ),
+            )
+        ),
+        templatePath = "components/forms/form.hbs",
+    ) { data -> PickEventSettlementResult(
+        settlementId = data.settlementId,
+        secretLocation = data.secretLocation,
+    )}
+}
