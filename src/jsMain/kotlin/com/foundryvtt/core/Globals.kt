51a60fc8abbcd7dfeda34a439d62ae5235909420
Bernhard Posselt <bernhard@posselt.at>
2025-04-18 22:44:19 +0200

    more moving things around

--- src/jsMain/kotlin/com/foundryvtt/core/Globals.kt
+++ src/jsMain/kotlin/com/foundryvtt/core/Globals.kt
@@ -2,3 +2,4 @@ package com.foundryvtt.core
 
 external val game: Game
 external val ui: Ui
+external val CONFIG: Config
--- src/jsMain/kotlin/com/foundryvtt/pf2e/Config.kt
+++ src/jsMain/kotlin/com/foundryvtt/pf2e/Config.kt
@@ -1,7 +1,12 @@
 package com.foundryvtt.pf2e
 
 import com.foundryvtt.core.Config
-import com.foundryvtt.core.PF2EActorConfig
+import com.foundryvtt.core.documents.Actor
+import js.objects.ReadonlyRecord
+
+external interface PF2EActorConfig {
+    val documentClasses: ReadonlyRecord<String, JsClass<Actor>>
+}
 
 @Suppress("UNCHECKED_CAST_TO_EXTERNAL_INTERFACE")
 inline val Config.PF2E: PF2EConfig
